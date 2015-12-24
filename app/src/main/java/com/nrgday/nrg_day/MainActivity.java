aab8d614c68c748464b3b40afb7073dd622c9299
nogardj3 <wwwfy@naver.com>
2015-12-24 18:22:49 +0900

    Update MainActivity.java

--- app/src/main/java/com/nrgday/nrg_day/MainActivity.java
+++ app/src/main/java/com/nrgday/nrg_day/MainActivity.java
@@ -41,10 +41,13 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        id;
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            
             return true;
+            
         }
 
         return super.onOptionsItemSelected(item);
